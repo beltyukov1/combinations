d25fc61a7b700cd3d4d119742049832bed4a2408
Alex Beltyukov <beltyukov.1@gmail.com>
2015-02-27 16:14:59 -0600

    Refactor

--- src/main/groovy/me/beltyukov/combinations/Combination.groovy
+++ src/main/groovy/me/beltyukov/combinations/Combination.groovy
@@ -8,16 +8,16 @@ class Combination {
         if (!args || args.size() != 2) {
             println "Usage: `./combinations <initial set> <desired combination length>`"
         } else {
-            def initialVector = args?.first()?.split(',')
-            def combinationSet = args[1] as Integer
+            def initialSet = args?.first()?.split(',')
+            def combinationLength = args[1] as Integer
 
-            getAllPossibleCombinations(initialVector, combinationSet)
+            getAllPossibleCombinations(initialSet, combinationLength)
         }
     }
 
-    static List getAllPossibleCombinations(String[] initialVector, Integer combinationSet) {
-        def combinatoricsVector = Factory.createVector(initialVector)
-        def generator = Factory.createSimpleCombinationGenerator(combinatoricsVector, combinationSet)
+    static List getAllPossibleCombinations(String[] initialSet, Integer combinationLength) {
+        def combinatoricsVector = Factory.createVector(initialSet)
+        def generator = Factory.createSimpleCombinationGenerator(combinatoricsVector, combinationLength)
         println "SUCCESS! Total combinations generated: ${generator.size()}"
         generator.collect {
             println it.vector
